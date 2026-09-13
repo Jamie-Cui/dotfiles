c900751497252b4373df2accd3bbb8d98a4f06a3
Jamie <jamie.cui@outlook.com>
2026-09-14 00:31:07 +0800

    refactor(emacs): consolidate paper feed sources

--- packages/emacs/.emacs.d/lisp/modules/reading.el
+++ packages/emacs/.emacs.d/lisp/modules/reading.el
@@ -18,9 +18,7 @@
                   ;;
                   ;; ------ Security ------
                   ;; crypto paper feeds
-                  ;; ("https://jamie-cui.github.io/paper-feeds/feed.xml")
-                  ("https://jamie-cui.github.io/paper-feeds/feed-arxiv.xml" summary)
-                  ("https://jamie-cui.github.io/paper-feeds/feed-iacr.xml" summary)
+                  ("https://feeds.jamie-cui.com/feed.xml")
                   ;; iacr paper
                   ("https://eprint.iacr.org/rss/rss.xml")
                   ;; Linux Security
